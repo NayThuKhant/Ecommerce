811e32b7b1c6a2c23f7cefa4ad730dd45a35beab
Khant Zaw Hein <kzh702@gmail.com>
2020-08-25 02:15:23 +0630

    User Model Fillable updated

--- app/User.php
+++ app/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'firebase_uid'
+        'name', 'email', 'password', 'phone', 'firebase_uid'
     ];
 
     /**
